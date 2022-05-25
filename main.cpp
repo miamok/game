766153192d32c462519764e860773618d349c894
miamok <90133394+miamok@users.noreply.github.com>
2022-05-25 21:01:51 +0600

    game
    mygame

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
+#include "mainwindow.h"
+#include <QApplication>
 
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setStyleSheet("background-color: rgb(210, 100, 140);");
+    w.show();
+
+
+    return a.exec();
+}
